d4c90bf78ecf1bb5fdec6e6ed188c031dc717c44
stellagqy <stellaqyg@gmail.com>
2024-10-15 08:55:44 +0800

    I tried my best

--- tree_drawing/tree.py
+++ tree_drawing/tree.py
@@ -2,16 +2,20 @@
 
 from turtle import *
 # 💻 IMPORT MODULES BELOW  💻 #
-
+from tree_parts import tree_top, tree_trunk
 
 
 
 # 💻 WRITE THE tree_full() FUNCTION BELOW  💻 #
-
-
-
+def tree_full(size):
+    tree_trunk(size)
+    penup()
+    back(25)
+    pendown()
+    tree_top(size)
+    
 
 
 # 💻 DON'T FORGET TO CALL THE FUNCTION BELOW  💻 #
-
-
+tree_full(100)
+input()
